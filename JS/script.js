453bb07f60de3eb7512e2a41b13a64fd23756fef
Curtis Myers <curt.myers@aol.com>
2022-01-07 00:33:39 -0500

    Made a Christmas tree project in HTML5, CSS and Jquery

---
+++ JS/script.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function(){
+$("button").click(function(){
+$(".let-there-be-light").animate({
+opacity: 1
+},6000);
+});
+});
+
+$(document).ready(function(){
+    $("button").click(function(){
+    $(".let-there-be-light").animate({
+    opacity: 0
+    },6000);
+    });
+    });
+
+    $(document).ready(function(){
+        $("button").click(function(){
+        $(".let-there-be-light").animate({
+        opacity: 1
+        },6000);
+        });
+        }); 
+                
+                    $(document).ready(function(){
+                        $("button").click(function(){
+                        $(".let-there-be-light").animate({
+                        opacity: 0
+                        },6000);
+                        });
+                        }); 
+           
+          
